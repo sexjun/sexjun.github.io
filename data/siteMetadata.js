f44d6d19d08fff2ffcf81a9e85cb619a9e4ef6c8
dsheng.chen <1196586501@qq.com>
2024-09-28 15:51:21 +0800

    delete un-used file

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,13 +1,13 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
-  language: 'en-us',
+  title: 'dsheng.chen Blog',
+  author: 'dsheng.chen',
+  headerTitle: 'Dsheng.chen Blog',
+  description: 'A blog about software engineering, programming, and web development.',
+  language: 'zh-CN,',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://sexjun-github-io.vercel.app/',
+  siteRepo: 'https://github.com/sexjun/sexjun.github.io',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   mastodon: 'https://mastodon.social/@mastodonuser',
